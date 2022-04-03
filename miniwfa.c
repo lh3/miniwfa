8911b8d9ec61e50b384fe85f435251edda6fc258
Heng Li <lh3@me.com>
2022-04-03 01:05:54 -0400

    also check length

--- miniwfa.c
+++ miniwfa.c
@@ -61,16 +61,21 @@ static void wf_cigar_push1(void *km, wf_cigar_t *c, int32_t op, int32_t len)
 }
 
 // recompute score from CIGAR
-int32_t mwf_cigar2score(const mwf_opt_t *opt, int32_t n_cigar, const uint32_t *cigar)
+int32_t mwf_cigar2score(const mwf_opt_t *opt, int32_t n_cigar, const uint32_t *cigar, int32_t *tl, int32_t *ql)
 {
-	int32_t k, s;
+	int32_t k, s, x = 0, y = 0;
 	for (k = 0, s = 0; k < n_cigar; ++k) {
 		int32_t op = cigar[k]&0xf, len = cigar[k]>>4;
 		if (op == 1 || op == 2) {
 			int32_t s1 = opt->o1 + len * opt->e1, s2 = opt->o2 + len * opt->e2;
 			s += s1 < s2? s1 : s2;
 		} else if (op == 8) s += opt->x;
+		if (op == 0 || op == 7 || op == 8) x += len, y += len;
+		else if (op == 1) y += len;
+		else if (op == 2) x += len;
 	}
+	if (tl) *tl = x;
+	if (ql) *ql = y;
 	return s;
 }
 
@@ -326,7 +331,12 @@ void mwf_wfa_basic(void *km, const mwf_opt_t *opt, int32_t tl, const char *ts, i
 	}
 	if (is_tb) {
 		r->cigar = wf_traceback(km, opt, &tb, tl-1, ts, ql-1, qs, &r->n_cigar);
-		if (opt->flag&BWF_F_DEBUG) fprintf(stderr, "s_ori=%d, s_recal=%d\n", r->s, mwf_cigar2score(opt, r->n_cigar, r->cigar));
+		if (opt->flag&BWF_F_DEBUG) {
+			int32_t s, x, y;
+			s = mwf_cigar2score(opt, r->n_cigar, r->cigar, &x, &y);
+			assert(tl == x && ql == y);
+			fprintf(stderr, "s_ori=%d, s_recal=%d\n", r->s, s);
+		}
 		km_destroy(km_tb);
 	}
 	wf_stripe_destroy(km, wf);
